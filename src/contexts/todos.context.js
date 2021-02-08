2a2c33167fa8bda7a2fb911ccbf077da522e9f21
ssmkhrj <iamssmkhrj@gmail.com>
2021-02-08 12:05:54 +0530

    Minor Changes

--- src/contexts/todos.context.js
+++ src/contexts/todos.context.js
@@ -2,7 +2,6 @@ import { createContext, useReducer } from "react";
 import todosReducer from "../reducers/todos.reducer";
 
 export const TodosContext = createContext();
-export const DispatchContext = createContext();
 
 export function TodosProvider(props) {
   const [todos, dispatch] = useReducer(todosReducer, []);
